35befa4d1da1413f97b96fa4390d91a23006a739
joasdc <idelfonsojoas@gmail.com>
2022-06-09 12:39:28 -0300

    update transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -6,58 +6,85 @@ import '../main.dart';
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
-  const TransactionList(this.transactions);
+  const TransactionList(this.transactions, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {
-        return Card(
-          child: Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
+    return Container(
+      height: 300,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "Nenhuma Transação Cadastrada",
+                  style: Theme.of(context).appBarTheme.titleTextStyle,
                 ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: HomePage.primaryColor,
-                    width: 2,
-                  ),
-                ),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  "R\$ ${transaction.value.toStringAsFixed(2).replaceAll('.', ',')}",
-                  style: const TextStyle(
-                    color: HomePage.primaryColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    // Sleep icons created by Freepik - Flaticon
+                    "assets/images/sleep.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    transaction.title,
-                    style: const TextStyle(
-                      color: HomePage.grayTitleText,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                final transaction = transactions[index];
+                return Card(
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: ExpensesApp.primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "R\$ ${transaction.value.toStringAsFixed(2).replaceAll('.', ',')}",
+                          style: const TextStyle(
+                            color: ExpensesApp.primaryColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              transaction.title,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                color: ExpensesApp.grayTitleText,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              DateFormat('d MMM y').format(transaction.date),
+                              style: const TextStyle(
+                                  color: ExpensesApp.graySubtitleText),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    DateFormat('d MMM y').format(transaction.date),
-                    style: const TextStyle(color: HomePage.graySubtitleText),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                );
+              },
+            ),
     );
   }
 }
